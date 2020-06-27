6cb4576718fe925259552e631a0fbf5afa82d20d
Serag Mounir <seragmounir7@gmail.com>
2020-06-27 12:53:38 +0200

    finishing up header with the responsive view

--- wp-content/themes/up-theme/footer.php
+++ wp-content/themes/up-theme/footer.php
@@ -1,4 +1,15 @@
         <?php wp_footer(); ?>
         <script src="<?php bloginfo('template_directory'); ?>/assets/js/jquery.js"></script>
+        <script src="https://kit.fontawesome.com/087e37f7bd.js" crossorigin="anonymous"></script>
+        <script>
+            //to open the navbar in responsive view
+            $('header .fa-bars').on('click',function(){
+                $('#responsive-navbar').addClass('active');
+            });
+            //to close the navbar in responsive view
+            $('header .fa-times').on('click',function(){
+                $('#responsive-navbar').removeClass('active');
+            });
+        </script>
     </body>
 </html>
--- wp-content/themes/up-theme/header.php
+++ wp-content/themes/up-theme/header.php
@@ -6,14 +6,23 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>u-advertise</title>
     <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/css/style.css?ver=1.1">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500;900&display=swap" rel="stylesheet">
     <?php wp_head(); ?>
 </head>
 <body>
     <header>
         <div class="container">
             <div class="display-flex justify-content-space-between align-items-center">
-                <img src="<?php bloginfo('template_directory'); ?>/assets/img/logo.png" alt="">
-                <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+                <a href="#">
+                    <img src="<?php bloginfo('template_directory'); ?>/assets/img/logo.png" alt="">
+                </a>
+                <div id="responsive-navbar">
+                    <div class="display-flex justify-content-flex-end">
+                        <i class="fas fa-times"></i>
+                    </div>
+                    <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+                </div>
+                <i class="fas fa-bars"></i>
             </div>
         </div>
     </header>
